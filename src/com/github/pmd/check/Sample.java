6f1d79391a5549f9b9e7935519d20e0e938304d6
Linus Fernandes <linus.fernandes@gmail.com>
2023-04-18 21:12:22 +0530

    first commit

---
+++ src/com/github/pmd/check/Sample.java
@@ -0,0 +1,10 @@
+package com.github.pmd.check;
+
+public final class Sample {
+  private Sample() {
+  }
+
+  public static void main(String... args) {
+    System.out.println("Hello, world!");
+  }
+}
